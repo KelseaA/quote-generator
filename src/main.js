6fc0ada574cb941f89b2b6a290ad22e6c30de96f
Kelsea Anderson <andersonkelsea@gmail.com>
2018-10-07 16:57:52 -0500

    start vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-// http://colormind.io/api-access/
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
 
 var $quoteList = [],
     $quote = $('.js-quote'),
